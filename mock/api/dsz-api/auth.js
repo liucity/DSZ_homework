3695260d146ac2a13b31868d2307b61bbfb963bd
Carson <liucity@126.com>
2023-10-16 00:03:53 +0800

    release

--- mock/api/dsz-api/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  return {
-    "iat": 1692143481,
-    "exp": 1692172281,
-    "token":`eyJhbGci0iJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRIC3RFYNib3VudEBkc3ouY29tLmF1IiwidH1wZSI6IiEiLCJyb2x1X21kIjozLCJpYX0i0iE20TIXNDM00DAsImV4cCI6MTY5MiE3MjI4MH0.Ai-eFg9J1mhvkf-norC5w0ExZzTxw0c50XIk72fphAs`
-  };
-}
